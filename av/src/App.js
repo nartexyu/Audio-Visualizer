615a9c11b29e2ab7a5546b9fe5a309b39b7bd0c8
echin522 <echin522@gmail.com>
2022-11-20 22:04:58 -0800

    Test commit

--- av/src/App.js
+++ av/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React pog
         </a>
       </header>
     </div>
